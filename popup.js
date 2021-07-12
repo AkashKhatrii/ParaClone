2c32b34680846db71f4ba01c178a6d7469741a7f
AkashKhatrii <65164913+AkashKhatrii@users.noreply.github.com>
2021-07-12 23:17:47 +0530

    Add edit save and delete options

--- popup.js
+++ popup.js
@@ -1,27 +1,115 @@
 let content = document.getElementById('content')
-let nameI = document.getElementById('name')
+let userQuery = document.getElementById('userQueryInput')
+let btn = document.getElementById('categoryButton')
+let ok = document.getElementById('Ok')
 let searchI = document.getElementById('search')
 let save_btn = document.getElementById('save-btn')
 let search_btn = document.getElementById('search-btn');
-console.log("Hey");
+let append_btn = document.getElementById('append-btn');
 
-save_btn.addEventListener("click", () =>{
+
+window.onload = (e) => {
+	content.disabled = true;
+    userQuery.disabled = true;
     
-    let myContentName = nameI.value;
-    let myContent = content.value;
-    localStorage.setItem(myContentName, myContent);
-    console.log(localStorage.getItem(myContentName));
-
-})
+    btn.style.display = 'none'
+}
 
 
-search_btn.addEventListener("click", () => {
-    if(!searchI.value)
-    {
-        console.log("No");
+ok.addEventListener("click", () => {
+    if(document.getElementById('new').checked){
+        content.disabled = false;
+        userQuery.disabled = false;
+        ok.style.display = 'none'
+        btn.innerHTML = "Save"
+        btn.style.display = 'initial'
+
+        btn.addEventListener("click", () => {
+            let userContent = content.value;
+            let fileName = userQuery.value;
+            localStorage.setItem(fileName, userContent);
+            console.log(localStorage.getItem(fileName))
+        })
     }
+
+    else if(document.getElementById('edit').checked){
+        userQuery.disabled = false;
+        ok.style.display = 'none'
+        btn.innerHTML = "Edit"
+        btn.style.display = 'initial'
+
+        btn.addEventListener("click", () => {
+            let fileName = userQuery.value;
+            if(fileName == null)
+            {
+                console.log("Please mention Content Name")
+            }
+            else{
+                var findContent = localStorage.getItem(fileName)
+                if(findContent == null){
+                    console.log("Sorry, there is no content named as such")
+                }
+
+                else{
+                    content.disabled = false;
+                    content.innerText = findContent;
+                    btn.innerHTML = "Confirm"
+                    btn.addEventListener("click", () => {
+                        localStorage.removeItem(fileName)
+                        let newContent = content.value
+                        localStorage.setItem(fileName, newContent)  // Here content.value will be the new value edited by the user
+                        console.log(localStorage.getItem(fileName))
+                    })
+                }
+            }
+        })
+
+    }
+
+    else if(document.getElementById('delete').checked){
+        userQuery.disabled = false;
+        ok.style.display = 'none'
+        btn.innerHTML = "Delete"
+        btn.style.display = 'initial'
+
+        btn.addEventListener("click", () => {
+            let fileName = userQuery.value;
+            let findContent = localStorage.getItem(fileName)
+            if(findContent == null){
+                console.log("There is no file named such.")
+            }
+            else{
+                localStorage.removeItem(fileName)
+                console.log("Deleted")
+            }
+
+        })
+
+    }
+
     else{
-        content.innerText = localStorage.getItem(searchI.value);
+        console.log("Please select a option")
     }
 })
+
+
+
+// save_btn.addEventListener("click", () =>{
+//     let myContentName = nameI.value;
+//     let myContent = content.value;
+//     localStorage.setItem(myContentName, myContent);
+//     console.log(localStorage.getItem(myContentName));
+
+// })
+
+
+// search_btn.addEventListener("click", () => {
+//     if(!searchI.value)
+//     {
+//         console.log("No");
+//     }
+//     else{
+//         content.innerText = localStorage.getItem(searchI.value);
+//     }
+// })
 
